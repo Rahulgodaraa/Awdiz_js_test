ebfb3423b6a9f91610ff22ceea76ce81ff56da6d
Rahul choudhary <rgodara152001@gmail.com>
2024-08-03 21:42:13 +0530

    comits

---
+++ 3/08/test2.js
@@ -0,0 +1,40 @@
+// 
+
+//q2
+
+const library = {
+    books: [
+    {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    copies: [
+    { copyNumber: 1, isAvailable: false },
+    { copyNumber: 2, isAvailable: true }
+    ]
+    },
+    {
+   title: "1984",
+    author: "George Orwell",
+    copies: [
+    { copyNumber: 1, isAvailable: false },
+    { copyNumber: 2, isAvailable: false }
+    ]
+    },
+    {
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    copies: [
+    { copyNumber: 1, isAvailable: true }
+    ]
+    }
+    ]
+    };
+
+    const newarr = [];
+
+    availableBook = (library) => {
+         
+    }
+
+    availableBook(library);
+
